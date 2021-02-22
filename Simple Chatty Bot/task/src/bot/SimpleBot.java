7d0f10854b690aba69d789da281b78f35ffacc68
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-02-22 18:34:23 +0100

    Initial commit

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -2,6 +2,6 @@ package bot;
 
 public class SimpleBot {
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("Hello! My name is Owl\n I was created in 2021");
     }
 }
