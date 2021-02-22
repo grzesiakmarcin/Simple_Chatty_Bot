5cf89aa380dcf11e9dd1ba8e49d7904b4d026b77
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-02-22 21:52:51 +0100

    nadal lecimy

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -10,8 +10,16 @@ public class SimpleBot {
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        String inputName = scanner.nextLine();
+        String name = scanner.nextLine();
 
-        System.out.println("What a great name you have, "+inputName+"!");
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int input1 = scanner.nextInt();
+        int input2 = scanner.nextInt();
+        int input3 = scanner.nextInt();
+        int age = (input1*70+input2*21+input3*15)%105;
+        System.out.println("Your age is "+age+"; that's a good time to start programming!");
     }
 }
