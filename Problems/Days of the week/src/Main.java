42f3b9bf5d2cadc05051b1e1bdcfdd03a3efa9cc
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-02-23 21:53:13 +0100

    Illegal Argument exception

---
+++ Problems/Days of the week/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+
+    public static String getDayOfWeekName(int number) throws IllegalArgumentException {
+        String day = "";
+
+
+        switch (number) {
+            case 1:
+                day = "Mon";
+                break;
+            case 2:
+                day = "Tue";
+                break;
+            case 3:
+                day = "Wed";
+                break;
+            case 4:
+                day = "Thu";
+                break;
+            case 5:
+                day = "Fri";
+                break;
+            case 6:
+                day = "Sat";
+                break;
+            case 7:
+                day = "Sun";
+                break;
+            default:
+                throw new IllegalArgumentException();
+
+        }
+        return day;
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dayNumber = scanner.nextInt();
+        try {
+            System.out.println(getDayOfWeekName(dayNumber));
+        } catch (Exception e) {
+            System.out.println(e.getClass().getName());
+        }
+    }
+}
