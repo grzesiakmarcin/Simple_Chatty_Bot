289ebba608758de5735a9790443455bb2448db3f
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-02-23 21:34:31 +0100

    jest to mały kroczek dla mnie i żaden kroczek dla programistycznego świata

--- Problems/Palindrome/src/Main.java
+++ Problems/Palindrome/src/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
 
         String input = sc.next();
 
@@ -10,14 +11,13 @@ class Main {
         int length = array.length;
         int result = 0;
 
-        for (int i =0; i<length;i++){
-            if(array[i]==array[(array.length-1)-i]){
+        for (int i = 0; i < length; i++) {
+            if (array[i] == array[(array.length - 1) - i]) {
                 result++;
             }
         }
 
-
-        System.out.println(result==length?"yes":"no");
+        System.out.println(result == length ? "yes" : "no");
 
     }
 }
