b5e4aed1a6d758ec7f80d08920ff091a6067e298
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-02-27 10:33:24 +0100

    no kurwa... Poszło

--- Problems/The fair exchange/src/Main.java
+++ Problems/The fair exchange/src/Main.java
@@ -4,21 +4,21 @@ import java.util.LinkedList;
 class ListOperations {
     public static void changeHeadsAndTails(LinkedList<String> linkedList, ArrayList<String> arrayList) {
 
-      String firstFromLinked = linkedList.getFirst();
-      String lastFromLinked = linkedList.getLast();
-      String firstFromArray = arrayList.get(0);
-      String lastFromArray = arrayList.get(arrayList.size()-1);
+        String firstFromLinked = linkedList.getFirst();
+        String lastFromLinked = linkedList.getLast();
+        String firstFromArray = arrayList.get(0);
+        String lastFromArray = arrayList.get(arrayList.size() - 1);
 
         linkedList.remove(0);
-        linkedList.add(0,firstFromArray);
+        linkedList.add(0, firstFromArray);
 
-        linkedList.remove(linkedList.size()-1);
+        linkedList.remove(linkedList.size() - 1);
         linkedList.add(lastFromArray);
 
         arrayList.remove(0);
-        arrayList.add(0,firstFromLinked);
+        arrayList.add(0, firstFromLinked);
 
-        arrayList.remove(arrayList.size()-1);
+        arrayList.remove(arrayList.size() - 1);
         arrayList.add(lastFromLinked);
 
 
---
+++ Problems/Transfer both lists/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+class ListOperations {
+    public static void transferAllElements(LinkedList<String> linkedList, ArrayList<String> arrayList) {
+
+        LinkedList<String> copiedLinkedList = new LinkedList<String>(linkedList);
+        ArrayList<String> copiedArrayList = new ArrayList<String>(arrayList);
+
+
+        arrayList.clear();
+
+        for (int i =0; i<copiedLinkedList.size();i++){
+            arrayList.add(i,copiedLinkedList.get(i));
+        }
+
+        linkedList.clear();
+        for (int i =0; i<copiedArrayList.size();i++){
+            linkedList.add(i,copiedArrayList.get(i));
+        }
+
+    }
+}
