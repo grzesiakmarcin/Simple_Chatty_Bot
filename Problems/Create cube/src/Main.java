17ee46494a1117dc4a00fb80c58dae69916072eb
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-03-03 16:01:00 +0100

    poszł0o

---
+++ Problems/Create cube/src/Main.java
@@ -0,0 +1,12 @@
+class ArrayOperations {
+    public static int[][][] createCube() {
+        int[][][] arr = {{{0,1,2}, {3,4,5},{6,7,8}},
+                         {{0,1,2}, {3,4,5},{6,7,8}} ,
+                         {{0,1,2}, {3,4,5},{6,7,8}}};
+
+
+
+
+return arr;
+    }
+}
