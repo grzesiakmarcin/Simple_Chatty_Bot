519cb2b40e381a5f29838121ffe992a11465bdfb
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-03-03 16:08:29 +0100

    poszł0o

--- Problems/Create cube/src/Main.java
+++ Problems/Create cube/src/Main.java
@@ -5,6 +5,17 @@ class ArrayOperations {
                          {{0,1,2}, {3,4,5},{6,7,8}}};
 
 
+//        int [][][] myArr = new int [3][3][3];
+//
+//for(int i=0; i<myArr.length;i++){
+//    for (int j=0;j< myArr[i].length;j++){
+//        for (int k=0; k< myArr[i][j].length;k++){
+//            myArr [i][j][k]=j*3+k;
+//        }
+//    }
+//}
+
+
 
 
 return arr;
