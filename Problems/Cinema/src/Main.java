a073e4794e1c990c537bb03c68c2e921ea50697e
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-03-06 20:20:43 +0100

    problem of the day

---
+++ Problems/Cinema/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int column = sc.nextInt();
+
+        int[][] cinema = new int[rows][column];
+
+        for (int i = 0; i < cinema.length; i++) {
+            for (int j = 0; j < cinema[i].length; j++) {
+                cinema[i][j] = sc.nextInt();
+            }
+        }
+
+
+        int howManyTickers = sc.nextInt();
+        int counter = 0;
+
+        int[][] zlicz = new int[rows][1];
+
+
+        for (int i = 0; i < cinema.length; i++) {
+            for (int j = 0; j < cinema[i].length; j++) {
+
+
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
+
+
