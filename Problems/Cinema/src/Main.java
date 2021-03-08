d40abefe2f8b9fc895cc969f55666dd8fbfcfc63
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-03-08 12:43:19 +0100

    wywalilo sie na tescie nr 3

--- Problems/Cinema/src/Main.java
+++ Problems/Cinema/src/Main.java
@@ -18,26 +18,46 @@ class Main {
 
         int howManyTickers = sc.nextInt();
         int counter = 0;
-
-        int[][] zlicz = new int[rows][1];
+        int moreThanOne = 0;
+        int answer = 0;
+        int currentRow = 0;
 
 
         for (int i = 0; i < cinema.length; i++) {
-            for (int j = 0; j < cinema[i].length; j++) {
+            for (int j = 0; j < cinema[i].length - 1; j++) {
 
+                if (i != currentRow) {
+                    counter = 0;
+                    moreThanOne = 0;
+                    currentRow++;
+                }
+
+                if (cinema[i][j] == 0) {
+                    counter++;
+                    if (cinema[i][j + 1] == 0) {
+
+                        moreThanOne++;
+
+
+                        if (moreThanOne + 1 >= howManyTickers) {
+
+                            System.out.println(+(currentRow + 1));
+                            break;
+
+                        }
+
+                    }
+                }
 
 
             }
 
+
         }
 
-
-
-
-
-
-
-
+        if(moreThanOne + 1 <howManyTickers){
+            System.out.println(0);
+        }
     }
 }
 
