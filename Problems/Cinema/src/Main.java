67839069d272a279a553166a41ead93c1d8c2faf
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-03-10 11:23:09 +0100

    ostatni test do zdania

--- Problems/Cinema/src/Main.java
+++ Problems/Cinema/src/Main.java
@@ -20,66 +20,70 @@ class Main {
 
         int currentRow = 0;
         boolean stopSearching = false;
-        boolean anotherSeatIsFree=false;
+        boolean anotherSeatIsFree = false;
         int seekedRow = 0;
-        int max=-1;
+        int max = -1;
 
+        //================== dla jednego===============
+        if (howManyTickersNeeded == 1) {
 
-        for (int i = 0; i < cinema.length; i++) {
-            for (int j = 0; j < cinema[i].length - 1; j++) {
-
-                if (howManyTickersNeeded == 1) {
+            for (int i = 0; i < cinema.length; i++) {
+                for (int j = 0; j < cinema[i].length; j++) {
                     if (cinema[i][j] == 0) {
 
                         if (stopSearching != true) {
                             seekedRow = (currentRow + 1);
+                            stopSearching = true;
                         }
-                        stopSearching = true;
                     }
-                } else {
+
+                }
+                currentRow++;
+            }
+
+        } else {
+
+            //   ===================dla wielu=================
+
+            for (int i = 0; i < cinema.length; i++) {
+                for (int j = 0; j < cinema[i].length - 1; j++) {
 
 
-                    if (i != currentRow) {
-                        avaliableSeatsNextToEachOther = 0;
-                        currentRow++;
+                    anotherSeatIsFree = cinema[i][j] == 0 && cinema[i][j + 1] == 0;
+
+                    if (anotherSeatIsFree == true) {
+
+                        avaliableSeatsNextToEachOther++;
+
+                        if (avaliableSeatsNextToEachOther == 1) {
+                            avaliableSeatsNextToEachOther++;
+                        }
+
+
+                        if (avaliableSeatsNextToEachOther > max) {
+                            max = avaliableSeatsNextToEachOther;
+                        }
+
+
+                        if (max == howManyTickersNeeded && stopSearching != true) {
+                            seekedRow = (currentRow + 1);
+
+                            stopSearching = true;
+                        }
+
 
                     } else {
-
-                        anotherSeatIsFree = cinema[i][j] == 0 && cinema[i][j+1] == 0;
-
-
-                        if (anotherSeatIsFree == true) {
-
-                            avaliableSeatsNextToEachOther++;
-
-                            if(avaliableSeatsNextToEachOther==1){
-                                avaliableSeatsNextToEachOther++;}
-
-
-                            if (avaliableSeatsNextToEachOther > max) {
-                                max = avaliableSeatsNextToEachOther;
-                            }
-
-                            if(max==howManyTickersNeeded&& stopSearching!=true){
-                                seekedRow=(currentRow+1);
-
-                                stopSearching=true;
-                            }
-
-
-
-                        } else {
-                            avaliableSeatsNextToEachOther = 0;
-                        }
-
+                        avaliableSeatsNextToEachOther = 0;
                     }
+
 
 
                 }
 
+                currentRow++;
+
             }
         }
-
 
         System.out.println(seekedRow);
 
