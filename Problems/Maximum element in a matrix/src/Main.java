8ad64fccd0a910d71ca5651da58a35d4838cfafb
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-03-10 13:14:45 +0100

    poszlo

--- Problems/Maximum element in a matrix/src/Main.java
+++ Problems/Maximum element in a matrix/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int column = sc.nextInt();
+
+        int[][] array = new int[rows][column];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = sc.nextInt();
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        int positionN = Integer.MAX_VALUE;
+        int positionM = Integer.MAX_VALUE;
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = array[i].length - 1; j >= 0; j--) {
+
+
+                if (array[i][j] >= max) {
+                    max = array[i][j];
+                    positionN = i;
+                    positionM = j;
+
+
+                }
+            }
+
+        }
+
+        System.out.println(positionN + " " + positionM);
+
     }
 }
